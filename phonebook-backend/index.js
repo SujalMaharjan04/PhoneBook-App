121df8d858059bcab07afac3f0df0b73874ae262
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-07-03 13:24:27 +0545

    Error Fix

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-app.use(express.statis("dist"));
+app.use(express.static("dist"));
 
 
 let data =[
