9128884026426e5183bc9101795b32d36c565109
SujalMaharjan04 <maharjan.sujal100@gmail.com>
2025-07-03 13:28:27 +0545

    Few Changes to the port

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -114,7 +114,7 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
